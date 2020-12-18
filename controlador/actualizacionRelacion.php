8c306b2fb8c3cc41366dc15310775e5c3db629f5
@omarcasasolasmerida <omarcasasolasmerida@gmail.com>
2020-12-18 00:03:14 -0400

    first commit 2

--- controlador/actualizacionRelacion.php
+++ controlador/actualizacionRelacion.php
@@ -1,9 +1,25 @@
 <?php
-    if(isset($_REQUEST['relacionSE'])){
-        require_once("../modelo/sintomaEnfermedad.php");
-        $sintomaEnfermedad = new SintomaEnfermedad();
-        $respuesta = $sintomaEnfermedad->obtnerActualizacionesSE($_REQUEST['relacionSE']);
-        echo $respuesta;
-    }else{
-        "No se esta pasando los valores";
+    header("Content-Type: application/json");
+    require_once("../modelo/sintomaEnfermedad.php");
+    $sintomaEnfermedad = new SintomaEnfermedad();
+    switch ($_SERVER['REQUEST_METHOD']) {
+        case 'POST':
+            $_POST = json_decode(file_get_contents('php://input'),true);
+            $res = $sintomaEnfermedad ->obtnerActualizacionesSE($_POST['relacionSE']);
+            echo $res;
+            $sintomaEnfermedad ->cerrarConexion();
+            break;
+        default:
+            $res = json_encode(array('Error'=>"No se pudo mostrar"));
+            echo $res;
+            break;
     }
+
+    // if(isset($_REQUEST['relacionSE'])){
+    //     require_once("../modelo/sintomaEnfermedad.php");
+    //     $sintomaEnfermedad = new SintomaEnfermedad();
+    //     $respuesta = $sintomaEnfermedad->obtnerActualizacionesSE($_REQUEST['relacionSE']);
+    //     echo $respuesta;
+    // }else{
+    //     "No se esta pasando los valores";
+    // }
